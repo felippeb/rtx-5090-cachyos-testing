3c9339c19755c470cfa0c9dc78b904e9d88679cb
Felippe Burk <felippeburk@protonmail.com>
2026-06-13 18:11:47 -0600

    remove opencode

--- opencode-mem/loader.js
+++
@@ -1,2 +0,0 @@
-const { OpenCodeMemPlugin } = await import('file:///home/felippeb/.cache/opencode/packages/opencode-mem@latest/node_modules/opencode-mem/dist/plugin.js');
-export const opencodeMemPlugin = async (ctx) => OpenCodeMemPlugin(ctx);
